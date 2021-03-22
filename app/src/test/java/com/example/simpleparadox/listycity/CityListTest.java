5e80147c3ec0b156cfdbb1e0a4d6dc494410a008
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-03-21 18:48:58 -0600

    update test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -31,7 +31,15 @@ class CityListTest {
 
     @Test
     public void testRemove(){
-        
+        CityList cityList = mockCityList();
 
+        assertEquals(1, cityList.countCities());
+
+        City city = new City("Regina", "Saskatchewan");
+
+        cityList.add(city);
+        cityList.delete(city);
+        assertEquals(2, cityList.countCities());
+//        assertFalse(cityList.hasCity(new City("Regina", "Saskatchewan")));
     }
 }
